d781c9e0ce6dfb1be888b39c75f96b65eecdac87
Satyam Banwale <satyambanwale786@gmail.com>
2025-04-17 17:19:13 +0530

    removed extra code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,46 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { UserRole } from "./user.schema";
 
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post("manager")
-  addManager(@Body() data: any) {
-    return this.userService.create({ ...data, role: UserRole.MANAGER });
-  }
-
-  @Post("staff")
-  addStaff(@Body() data: any) {
-    return this.userService.create({ ...data, role: UserRole.STAFF });
-  }
-
-  @Patch(":id")
-  editUser(@Param("id") id: string, @Body() updates: any) {
-    return this.userService.update(id, updates);
-  }
-
-  @Delete(":id")
-  deleteUser(@Param("id") id: string) {
-    return this.userService.delete(id);
-  }
-
-  @Get("manager")
-  getAllManagers() {
-    return this.userService.findAllByRole(UserRole.MANAGER);
-  }
-
-  @Get("staff")
-  getAllStaff() {
-    return this.userService.findAllByRole(UserRole.STAFF);
-  }
 }
