3594f2b9b2f25a02d065315492aff0f43fa5d250
Satyam Banwale <satyambanwale786@gmail.com>
2025-04-18 23:31:18 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
