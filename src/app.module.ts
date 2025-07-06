c587601a59c787c24d5cfd51742c1e5f89107183
Satyam Banwale <satyambanwale786@gmail.com>
2025-07-06 23:35:43 +0530

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { AuthModule } from "./auth/auth.module";
 import { MachineModule } from "./machine/machine.module";
 import { FuelPriceModule } from "./fuel-price/fuel-price.module";
 import { PumpDataModule } from "./pump-data/pump-data.module";
-import { PlanSchedulerService } from "./common/plan-scheduler.service"; // Adjust path as needed
+import { PlanSchedulerService } from "./common/plan-scheduler.service";
 
 @Module({
   imports: [
