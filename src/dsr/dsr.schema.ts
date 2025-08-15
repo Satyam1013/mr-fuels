2ae7067ef496a28ffd555c1dbe8123cc0805f46f
Satyam <satyambanwale786@gmail.com>
2025-08-15 12:49:33 +0530

    optimize

--- src/dsr/dsr.schema.ts
+++ src/dsr/dsr.schema.ts
@@ -1,9 +1,12 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
 
 export type DSRDocument = DSR & Document;
 
 @Schema({ timestamps: true })
-export class DSR {}
+export class DSR {
+  @Prop({ type: Types.ObjectId, ref: "Admin", required: true })
+  pumpId!: Types.ObjectId;
+}
 
 export const DSRSchema = SchemaFactory.createForClass(DSR);
